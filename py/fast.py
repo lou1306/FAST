508b0ba775c3f791655e8b450127188f13c9373e
Luca Di Stefano <lucad@chalmers.se>
2022-11-23 13:00:43 +0100

    Here we go
    On the 1st run, we turn each test case into a CNF formula (via JBMC),
    do some reductions (via minisat) and store each clause as an element
    of the set that will be minhashed later. We also store some annotations
    from JBMC, telling us which SUT functions have been called by the TC
    and where. This will disambiguate two tests that are identical but call
    two different functions.
    
    We need to a classpath and the directory where to run JBMC. This is
    done by setting environment variables "FAST_JBMC_CP" and
    "FAST_JBMC_DIR" (either at the cmd line or in an .env file).

--- py/fast.py
+++ py/fast.py
@@ -16,15 +16,39 @@ You should have received a copy of the GNU General Public License
 along with this source.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from collections import defaultdict
-from collections import OrderedDict
-from struct import pack, unpack
 import os
 import random
+import re
+import subprocess
 import sys
 import time
+from collections import OrderedDict, defaultdict
+from concurrent.futures import ProcessPoolExecutor
+from functools import lru_cache
+from struct import pack, unpack
+from subprocess import DEVNULL, PIPE
+from tempfile import NamedTemporaryFile
+
+from dotenv import load_dotenv
 
 import lsh
+
+load_dotenv()
+# TODO eager check for required environment variables
+
+def process_clause(line):
+    if not line:
+        return None
+    elif line[0] not in ("c", "p"):
+        return tuple(
+            int(lit) for lit in line[:-1].split()
+            if lit != "0")
+
+
+def process_funccalls(line):
+    return line \
+    if line[0] == "c" and "return_value" in line \
+    else None
 
 
 def loadTestSuite(input_file, bbox=False, k=5):
@@ -51,27 +75,204 @@ def loadTestSuite(input_file, bbox=False, k=5):
     return TS
 
 
-def storeSignatures(input_file, sigfile, hashes, bbox=False, k=5):
-    with open(sigfile, "w") as sigfile:
-        with open(input_file) as fin:
-            tcID = 1
-            for tc in fin:
-                if bbox:
-                    # shingling
-                    tc_ = tc[:-1]
-                    tc_shingles = set()
-                    for i in range(len(tc_) - k + 1):
-                        tc_shingles.add(hash(tc_[i:i + k]))
+def loadSatTestSuite(input_file, bbox=False, k=5):
+    """INPUT
+    (str)input_file: path of input file
 
-                    sig = lsh.tcMinhashing((tcID, set(tc_shingles)), hashes)
-                else:
-                    tc_ = tc[:-1].split()
-                    sig = lsh.tcMinhashing((tcID, set(tc_)), hashes)
-                for hash_ in sig:
-                    sigfile.write(repr(unpack('>d', hash_)[0]))
-                    sigfile.write(" ")
-                sigfile.write("\n")
-                tcID += 1
+    OUTPUT
+    (dict)TS: key=tc_ID, val=set(covered lines)
+    """
+    TS = defaultdict()
+    with open(input_file) as fin:
+        for tcID, tc in enumerate(fin, 1):
+            if bbox:
+                TS[tcID] = tc[:-1]
+            else:
+                TS[tcID] = set(tc[:-1].split())
+    shuffled = list(TS.items())
+    # random.shuffle(shuffled)
+    TS = OrderedDict(shuffled)
+    if bbox:
+        TS = lsh.kShingles(TS, k)
+    return TS
+
+
+PACKAGE = re.compile(r"package ([A-Za-z0-9_.]+)")
+CLASS = re.compile(r"public( final)? class ([A-Za-z0-9_]+)")
+BOUND_WITH_UAS = 2
+
+@lru_cache(maxsize=8)
+def has_loops(entry_point):
+    cmd = [
+        "jbmc",
+        "-cp", os.environ["FAST_JBMC_CP"],
+        "--show-loops",
+        entry_point
+    ]
+    x = subprocess.run(cmd, encoding="utf8", cwd=os.environ["FAST_JBMC_DIR"], stdout=PIPE)
+    if x.returncode != 0:
+        print(x.returncode)
+        raise ValueError("Something bad happened while generating CNF")
+    return "file" in x.stdout and "line" in x.stdout
+
+
+def get_cnf(entry_point, dest_filename, unwind, *args):
+    timeout_value = 500
+    limit = 100
+    cmd = [
+        "jbmc",
+        "-cp", os.environ["FAST_JBMC_CP"],
+        "--unwind", str(unwind),
+        "--dimacs",
+        "--outfile", dest_filename,
+        "--java-assume-inputs-integral",
+        # "--java-assume-inputs-non-null",
+        "--disable-uncaught-exception-check",
+        # "--reachability-slice",
+        # "--string-printable",
+        # "--string-non-empty",
+        "--drop-unused-functions",
+        "--max-nondet-string-length", "0",
+        "--max-nondet-array-length", "0",
+        "--max-nondet-tree-depth", "1",
+        # "--symex-driven-lazy-loading",
+        # "--drop-unused-functions",
+        # "--slice-formula",
+        entry_point, *args]
+    try:
+        x = subprocess.run(
+            cmd,
+            cwd=os.environ["FAST_JBMC_DIR"],
+            timeout=timeout_value, stderr=DEVNULL, stdout=DEVNULL)
+        size = subprocess.check_output(["head", "-n", "1", dest_filename], encoding="utf8")
+        if x.returncode == -6:
+            # Invariant violation (happens sometimes)
+            # TODO get invoked functions with javalang
+            return "p cnf 99999999 99999999"
+        elif x.returncode != 0:
+            print(x.returncode)
+            raise ValueError("Something bad happened while generating CNF")
+    except subprocess.TimeoutExpired:
+        return "p cnf 99999999 99999999"
+        # Give up
+        # size = "p cnf 0 0"
+        # if "--slice-formula" not in args:
+        #     # Restart with slicing
+        #     return get_cnf(
+        #         entry_point, dest_filename, 1, 
+        #         *[*args, "--slice-formula"]
+        #     )
+        # else:
+
+    if "cnf 0 0" in size:
+        if not has_loops(entry_point):
+            print(f"No assertions found and no loops, giving up.")
+            return size
+        elif unwind < limit:
+            print(f"No assertions found for {unwind}, trying {unwind+1}...")
+            return get_cnf(entry_point, dest_filename, unwind+1, *args)
+        else:
+            if "--unwinding-assertions" not in args:
+                args = [*args, "--unwinding-assertions"]
+                print(
+                    f"No assertions found for {unwind}, "
+                    f"restart at {BOUND_WITH_UAS} with unwinding assertions.")
+                return get_cnf(entry_point, dest_filename, BOUND_WITH_UAS, *args)  # noqa: E501
+            else:
+                print(f"No assertions found for {unwind}, giving up.")
+    return size
+
+
+def storeSignatures(input_file, sigfile, hashes, bbox=False, k=5):
+    with (
+        open(sigfile, "w") as sigfile,
+        open(input_file) as fin,
+        NamedTemporaryFile(delete=False, suffix=".cnf") as cnf,
+        NamedTemporaryFile(delete=False, suffix=".cnf") as reduced_cnf,
+        ProcessPoolExecutor() as EXEC
+    ):
+        tcID = 1
+        cnf.close()
+        reduced_cnf.close()
+        for tc in fin:
+            if bbox:
+                # shingling
+                t = time.time()
+                tc_ = tc[:-1]   
+                pkg = PACKAGE.findall(tc_)[0]
+                cls = CLASS.findall(tc_)[0][1]
+                try:
+                    print(pkg, cls, "..", sep=".")
+                    size_original = get_cnf(f"{pkg}.{cls}.__TC", cnf.name, 1)
+                    # TODO if for whatever reason the CNF cannot be generated, 
+                    # we should prioritize the test. Sad, but sound
+                except ValueError as err:
+                    EXEC.shutdown()
+                    raise err
+                
+                def minisat_pre(elim=True):
+                    cmd = [
+                            "minisat",
+                            cnf.name,
+                            "-no-solve",
+                            f"-dimacs={reduced_cnf.name}"]
+                    if not elim:
+                        cmd.append("-no-elim")
+                    try:
+                        subprocess.check_output(cmd)
+                    except subprocess.CalledProcessError as err:
+                        # 10 (r. 20) just means minisat proved the cnf to be 
+                        # SAT (r. UNSAT) while preprocessing. Nothing bad
+                        if err.returncode not in (10, 20):
+                            EXEC.shutdown
+                            raise err
+
+                end_char = "" if size_original[-1] == '\n' else '\n'
+                print("Original CNF:          ", size_original, end=end_char)
+                minisat_pre()
+                # size_original = subprocess.check_output(["head", "-n", "1", cnf.name], encoding="utf8")
+                size_reduced = subprocess.check_output(["head", "-n", "1", reduced_cnf.name], encoding="utf8")
+                print("Reduced CNF:           ", size_reduced, end="")
+                if "cnf 0 0" in size_reduced:
+                    minisat_pre(elim=False)
+                    size_reduced = subprocess.check_output(["head", "-n", "1", reduced_cnf.name], encoding="utf8")
+                    print("Reduced CNF (no-elim): ", size_reduced, end="")
+
+
+                with open(cnf.name) as f_cnf, open(reduced_cnf.name) as f_r:
+                    tc_shingles = EXEC.map(process_clause, f_r, chunksize=10000)
+                    calls = EXEC.map(process_funccalls, f_cnf, chunksize=10000)
+
+                    tc_shingles = set(tc_shingles).union(calls)
+
+                    # Original ICSE'18 code. In memoriam
+                    # tc_shingles = set(c for c in clauses if c)
+                    # print(tc_shingles)
+                    # tc_shingles = set(
+                    #     tuple(
+                    #         int(lit) for lit in clause[:-1].split()
+                    #         if lit != "0"
+                    #     )
+                    #     for clause in cnf
+                    #     if clause and clause[0] not in ("c", "p")
+                    # )
+
+                sig = lsh.tcMinhashing((tcID, set(tc_shingles)), hashes)
+                print(time.time() - t, "seconds")
+                # print(sig)
+                # input()
+            else:
+                tc_ = tc[:-1].split()
+                sig = lsh.tcMinhashing((tcID, set(tc_)), hashes)
+                
+            for hash_ in sig:
+                sigfile.write(repr(unpack('>d', hash_)[0]))
+                sigfile.write(" ")
+            sigfile.write("\n")
+            tcID += 1
+
+        os.remove(cnf.name)
+        os.remove(reduced_cnf.name)
 
 
 def loadSignatures(input_file):
@@ -107,6 +308,8 @@ def fast_pw(input_file, r, b, bbox=False, k=5, memory=False):
     (list)P: prioritized test suite
     """
     n = r * b  # number of hash functions
+
+    print(input_file, r, b, bbox, k, memory)
 
     hashes = [lsh.hashFamily(i) for i in range(n)]
 
